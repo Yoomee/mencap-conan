6a2c3a10c254d729dc80484581ed5fde2fb05697
Andy Mayer <andymayer@Andy-Mayers-MacBook-Pro.local>
2012-10-31 22:58:16 +0000

    Fix malformed from address for exception notifier

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,6 +1,6 @@
 unless (Rails.env.development? || Rails.env.test?)
   MencapConan::Application.config.middleware.use ExceptionNotifier,
     :email_prefix => "[MENCAP CONAN] ",
-    :sender_address => "notifier <notifier@mencap_conan.yoomee.com>",
+    :sender_address => "notifier <notifier@conan.yoomee.com>",
     :exception_recipients => %w{developers@yoomee.com}
 end
