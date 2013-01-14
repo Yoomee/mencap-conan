68911756d1e99ec8ed3fd92c7a4a36435686b695
Edward Andrews <edward@yoomee.com>
2013-01-14 14:53:04 +0000

    Order pages by position

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   include YmCms::PagesController
   
+    def show
+      @page_children = @page.children.order(:position) || []
+      render :action => "views/#{@page.view_name}"
+    end
+    
 end
